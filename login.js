68f56fb91df4d748871437b0382c4958b5f0f4f1
be_nyantorusabu <nnya13586@gmail.com>
2025-07-16 18:10:52 +0900

    Update login.js

--- login.js
+++ login.js
@@ -81,7 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
             if (sessionError) throw new Error('セッションの設定に失敗しました。');
             
-            window.location.href = 'index.html';
+            window.location.replace('/');
 
         } catch (e) {
             errorMessage.textContent = e.message;
