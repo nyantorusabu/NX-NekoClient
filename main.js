eb76cfbe25661f5a569637805ae8799ed3d1dfcb
nyantorusabu <nyantorusabu2@outlook.jp>
2026-01-31 03:54:40 +0000

    UPD

--- main.js
+++ main.js
@@ -2633,7 +2633,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 if (user.lock) {
                     blockNoticeHtml += `<div class="freeze-notice">このユーザーはポストを非公開に設定しています。表示するにはフォローリクエストを送信してください。</div>`;
                 }
-            } else {
+            } else if (!currentUser) {
                 if (user.lock) {
                     blockNoticeHtml += `<div class="freeze-notice">このユーザーはポストを非公開に設定しています。</div>`;
                 }
